b3c97158aea2a2453f2d1fe96a8abbfb490d99c4
tatphong <50413495+tatphong@users.noreply.github.com>
2022-04-27 13:13:17 +0700

    add generatePDF

---
+++ GeneratePDFfromHTML/GeneratePDFfromHTML/Program.cs
@@ -0,0 +1,42 @@
+﻿using DinkToPdf;
+using System;
+
+namespace GeneratePDFfromHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public IAsyncResult DownloadPDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "Exam Report",
+                //Out = @"D:\PDFCreator\Employee_Report.pdf"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = htmlDisplayText,
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "", Line = false },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Trang [page]/[toPage]" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf");
+        }
+    }
+}
