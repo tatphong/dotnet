beba32bb8e5807fffa67af41b1c588d38f8d8e0a
tatphong <50413495+tatphong@users.noreply.github.com>
2022-05-24 15:13:57 +0700

    add ntp_syncTime

---
+++ NTP_SyncTime/NTP_Synch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace NTP_Synch
+{
+    class Program
+    {
+        public static DateTime GetNetworkTime1()
+        {
+            string ntpServer = ConfigurationManager.AppSettings.Get("NTPServer");
+            int ntpPort = Int32.Parse(ConfigurationManager.AppSettings.Get("NTPPort"));
+            Console.WriteLine(ntpServer);
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B; //LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+            var ipEndPoint = new IPEndPoint(addresses[0], ntpPort);
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+                Console.WriteLine("Connected.");
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 10000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+            Console.WriteLine("Closed.");
+            ulong intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | (ulong)ntpData[43];
+            ulong fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | (ulong)ntpData[47];
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            var networkDateTime = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
+            return networkDateTime;
+        }
+
+        static async Task Main(string[] args)
+        {
+            DateTime ntp_server = GetNetworkTime1();
+            DateTime now = DateTime.Now;
+            TimeSpan span = ntp_server - now;
+            double span_minu = span.Minutes + (span.Seconds * 1.0 / 60);
+            if (span.Hours != 0 || Math.Abs(span_minu) >= 5)
+                await new SendMail().SendMailAuto(span.Hours, Math.Abs(span_minu));
+
+            Console.WriteLine("Complete.");
+        }
+    }
+}
---
+++ NTP_SyncTime/NTP_Synch/SendMail.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP_Synch
+{
+    class SendMail
+    {
+        public async Task SendMailAuto(int hour, double min)
+        {
+            Console.WriteLine("Start send mail.");
+            string hostMail = ConfigurationManager.AppSettings.Get("hostMail");
+            string passEmail = ConfigurationManager.AppSettings.Get("passMail"); 
+            string destEmail = ConfigurationManager.AppSettings.Get("destMail");
+            try
+            {
+                System.Net.Mail.SmtpClient smtpClient = new System.Net.Mail.SmtpClient("192.168.8.4");
+                smtpClient.EnableSsl = false;
+
+                smtpClient.Credentials = new System.Net.NetworkCredential(hostMail, passEmail);
+
+                System.Net.Mail.MailMessage mailMsg = new System.Net.Mail.MailMessage();
+
+                mailMsg.To.Add(destEmail);
+                mailMsg.From = new System.Net.Mail.MailAddress(hostMail, hostMail);
+
+                mailMsg.Subject = "CẢNH BÁO LỆCH THỜI GIAN VỚI SERVER NTP";
+
+                string content = "Thời gian chênh lệch: " + hour + " giờ " + Math.Round(min) + " phút.";
+
+                //mailMsg.Body = content;
+                mailMsg.IsBodyHtml = true;
+                mailMsg.Body = content;
+                mailMsg.BodyEncoding = Encoding.Default;
+                mailMsg.Priority = MailPriority.High;
+                await smtpClient.SendMailAsync(mailMsg);
+                Console.WriteLine("Send mail success");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi gửi mail: " + ex);
+                Console.Read();
+            }
+        }
+    }
+}
